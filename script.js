c6c5cbbc18bc9880e9396adda4008680a6f57ed5
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-05-17 15:45:51 +0200

    feat: snack 2

--- script.js
+++ script.js
@@ -59,3 +59,28 @@ console.log(longBooksTitles);
 // Stampa in console ogni titolo nella console.
 
 books.forEach(book => console.log(book.title));
+
+
+//Creare un array (availableBooks) che contiene tutti i libri disponibili.
+
+const availableBooks = books.filter(book => book.available === true)
+
+console.log(availableBooks);
+
+
+//Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+
+const discountedBooks = availableBooks.map(book => {
+    return {
+        ...book,
+        price: (parseInt(book.price) * 0.8).toFixed(2)
+    };
+});
+
+console.log(discountedBooks);
+
+//Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+
+const fullPricedBook = discountedBooks.find(book => book.price % 1 === 0)
+
+console.log(fullPricedBook);
